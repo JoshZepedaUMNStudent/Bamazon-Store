ea67020e70d5f90bd2dad7325b65620759cbb4da
JZStudent <joshzepeda.student@gmail.com>
2018-12-30 13:38:13 -0600

    finalized

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var inquirer = require("inquirer");
 var cTable = require("console.table");
 
 var connection = mysql.createConnection({
-    host: "Localhost",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
